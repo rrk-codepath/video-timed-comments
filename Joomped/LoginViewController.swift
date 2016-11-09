8097cf5eb958b4153829b49b5276468b9fe4d621
Rahul Pandey <rahul@pinterest.com>
2016-11-08 20:56:31 -0800

    basic login working

--- Joomped/LoginViewController.swift
+++ Joomped/LoginViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("hello world")
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +25,52 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    @IBAction func didTapLogin(_ sender: Any) {
+        let user = try? PFUser.logIn(withUsername: usernameTextField.text!, password: passwordTextField.text!)
+        if let user = user {
+            print("logged in as: \(user.username)")
+            self.performSegue(withIdentifier: "HomeSegue", sender: self)
+        } else {
+            print("could not login: \(user)")
+            let alertController = UIAlertController(title: "Failure", message: "Failed to login", preferredStyle: .alert)
+            let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                // dismiss by default
+            }
+            alertController.addAction(OKAction)
+            present(alertController, animated: true, completion: {
+                // empty
+            })
+        }
+    }
+
+    @IBAction func didTapSignup(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameTextField.text
+        user.password = passwordTextField.text
+        //        user.email = "email@example.com"
+        
+        user.signUpInBackground {
+            (succeeded: Bool, error: Error?) -> Void in
+            if let error = error {
+                print("error: \(error.localizedDescription)")
+                // let errorString = error.userInfo["error"] as? String
+                // Show the errorString somewhere and let the user try again.
+                let alertController = UIAlertController(title: "Failure", message: "Failed to signup", preferredStyle: .alert)
+                let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+                    // dismiss by default
+                }
+                alertController.addAction(OKAction)
+                self.present(alertController, animated: true, completion: {
+                    // empty
+                })
+            } else {
+                self.performSegue(withIdentifier: "HomeSegue", sender: self)
+                // Hooray! Let them use the app now.
+                print("signed up")
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
