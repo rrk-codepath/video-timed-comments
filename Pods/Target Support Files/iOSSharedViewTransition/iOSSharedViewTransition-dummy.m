74fe02a99cb55d6acbd2fd3f3333595abb22616f
Rahul Pandey <rahul@pinterest.com>
2016-12-03 16:15:40 -0800

    pods for shared element transition

---
+++ Pods/Target Support Files/iOSSharedViewTransition/iOSSharedViewTransition-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iOSSharedViewTransition : NSObject
+@end
+@implementation PodsDummy_iOSSharedViewTransition
+@end
---
+++ Pods/Target Support Files/iOSSharedViewTransition/iOSSharedViewTransition-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "ASFSharedViewTransition.h"
+
+FOUNDATION_EXPORT double iOSSharedViewTransitionVersionNumber;
+FOUNDATION_EXPORT const unsigned char iOSSharedViewTransitionVersionString[];
+
