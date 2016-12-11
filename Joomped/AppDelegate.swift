7d25043298794c20f2e3aaa496b281ac4f796794
R-J Lim <rlim@zynga.com>
2016-12-10 21:40:50 -0800

    Refresh google token on app foreground

--- Joomped/AppDelegate.swift
+++ Joomped/AppDelegate.swift
@@ -57,7 +57,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        if PFUser.current() != nil {
+            GIDSignIn.sharedInstance().signInSilently()
+        }
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
